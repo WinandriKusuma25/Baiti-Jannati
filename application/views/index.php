0b3c6fd1b2e042a905b10f6ab7b178c3b6c22f42
muhammad misbaqul Ulum <muhammadmisbaqul@gmail.com>
2021-04-06 23:03:50 +0700

    Konten beranda

--- application/views/index.php
+++ application/views/index.php
@@ -346,102 +346,28 @@
                         <div class="accordion-item wow fadeInRight">
                             <div class="accordion-trigger" id="headingFive">
                                 <button class="btn" type="button" data-toggle="collapse" data-target="#collapse2"
-                                    aria-expanded="true" aria-controls="collapse2">Already seeing payment
-                                    methods</button>
+                                    aria-expanded="true" aria-controls="collapse2">Apa bisa Donatur berdonasi tanpa ke Yayasan?</button>
                             </div>
                             <div id="collapse2" class="collapse show" aria-labelledby="headingFive"
                                 data-parent="#accordionFAQ">
                                 <div class="accordion-content">
-                                    <p>New to Google Pay and already seeing payment methods? If you paid for
-                                        something
-                                        through Google in the past, Google Pay saved your card.</p>
+                                    <p>Bisa, karena bisa langsung melalui sistem dan terdapat beberapa metode pembayaran yang tersedia.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="accordion-item wow fadeInRight">
                             <div class="accordion-trigger" id="headingSix">
                                 <button class="btn collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">Your
-                                    transaction cannot be completed</button>
+                                    data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">Apakah bisa Donatur menyembunyikan nama Donatur untuk berdonasi? </button>
                             </div>
                             <div id="collapse3" class="collapse" aria-labelledby="headingSix"
                                 data-parent="#accordionFAQ">
                                 <div class="accordion-content">
-                                    <p>If you see this error message, wait 24 hours and try to add your card again.
+                                    <p>Bisa, Donatur bisa mengubahnya di fitur Edit Profile halaman member, misalnya bisa diubah menjadi Hamba Allah.
                                     </p>
                                 </div>
                             </div>
                         </div>
-                        <div class="accordion-item wow fadeInRight">
-                            <div class="accordion-trigger" id="headingSeven">
-                                <button class="btn collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapse4" aria-expanded="false" aria-controls="collapse4">Credit
-                                    &
-                                    Debit cards</button>
-                            </div>
-                            <div id="collapse4" class="collapse" aria-labelledby="headingSeven"
-                                data-parent="#accordionFAQ">
-                                <div class="accordion-content">
-                                    <ul>
-                                        <li>American Express</li>
-                                        <li>MasterCard</li>
-                                        <li>Visa</li>
-                                        <li>Discover (U.S. only)</li>
-                                        <li>JCB (Japan and U.S. only)</li>
-                                        <li>Visa Electron (outside U.S. only)</li>
-                                        <li>Elo credit cards (Brazil only; Elo debit cards aren’t accepted)</li>
-                                    </ul>
-                                    <p>The types of cards accepted by Google services vary by location and product.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="accordion-item wow fadeInRight">
-                            <div class="accordion-trigger" id="headingEight">
-                                <button class="btn collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapse5" aria-expanded="false" aria-controls="collapse5">Send
-                                    money
-                                    to friends or family</button>
-                            </div>
-                            <div id="collapse5" class="collapse" aria-labelledby="headingEight"
-                                data-parent="#accordionFAQ">
-                                <div class="accordion-content">
-                                    <p>This feature is only available in the US. For the US territories, this is
-                                        supported only if the debit card was issued under a US-licensed bank. If
-                                        you'd
-                                        like to know in advance whether your payment to or from a US territory will
-                                        go
-                                        through, please check with your card issuing bank.</p>
-
-                                    <p>You can use Google Pay to send money to friends and family or to someone
-                                        using
-                                        their email address or phone number. Google Pay charges no fees.</p>
-
-                                    <p>If you have a problem sending money, here is a list of likely reasons.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="accordion-item wow fadeInRight">
-                            <div class="accordion-trigger" id="headingNine">
-                                <button class="btn collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapse6" aria-expanded="false" aria-controls="collapse6">How
-                                    long it
-                                    usually takes to send or receive money</button>
-                            </div>
-                            <div id="collapse6" class="collapse" aria-labelledby="headingNine"
-                                data-parent="#accordionFAQ">
-                                <div class="accordion-content">
-                                    <p>For the best Google Pay experience, we recommend using a debit card whenever
-                                        you
-                                        send or receive money from friends and family. With a debit card, sending
-                                        and
-                                        receiving money is usually the fastest option.</p>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
             </div>
